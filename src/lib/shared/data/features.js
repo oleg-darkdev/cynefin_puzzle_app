51291ca4669f3c9788edd1760867906af79592de
DarkDev <darkdev_brand@riseup.net>
2024-10-05 18:14:47 +0300

    beta v. features section

---
+++ src/lib/shared/data/features.js
@@ -0,0 +1,23 @@
+ const features = [
+    {
+      title: 'Lorem Ipsum has been the industry',
+      desc: `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      img: '/images/621524e92f2a22c2e9db61ee_Instant%20answers%20icon_SlangCards.png',
+      brandColor: ''
+    },
+    {
+      title: 'Play and laearning.',
+      desc: `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      img: '/images/621524e92f2a220234db61f3_Save%20time%20icon_SlangCards.png',
+      brandColor: 'card_yellow'
+    },
+    {
+      title: 'Lorem Ipsum has been the industry',
+      desc: `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+      img: '/images/621524e92f2a222bb8db61eb_White%20glove%20support%20icon_SlangCards.png',
+      brandColor: 'card_magenta'
+    },
+]
+
+
+export default features;
---
+++ src/lib/widgets/ui/landing/Features.svelte
@@ -0,0 +1,28 @@
+<script>
+  import {features} from '$shared';
+</script>
+
+<div class="section">
+  <div class="container">
+    <div id='features' class="c-center_content">
+      <h2 class="margin-bottom-80">Lorem Ipsum is + simply dummy text = of the printing </h2>
+    </div>
+    <div class="grid-40">
+      {#each features as {title, desc, img, brandColor}}
+      <div class="content-layout-center card {brandColor}">
+        <img src="{img}" loading="lazy" width="180" alt="" class="margin-bottom-40 instant_answers">
+        <h3>{title}</h3>
+        <p class="margin-bottom-0">
+          {desc}
+        </p>
+      </div>
+      {/each}
+    </div>
+  </div>
+</div>
+
+
+<style>
+
+
+</style>
