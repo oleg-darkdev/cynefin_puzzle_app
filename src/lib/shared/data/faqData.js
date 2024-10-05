ca2efe2da8b5d6e27a520a51abec1ac8d29b7fe9
DarkDev <darkdev_brand@riseup.net>
2024-10-05 18:13:43 +0300

    upld data for faq

---
+++ src/lib/shared/data/faqData.js
@@ -0,0 +1,27 @@
+  const faqData = [
+    {
+      q: 'Lorem Ipsum is simply typesetting industry',
+      a: [`Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`, ``,]
+    },
+    {
+      q: 'Lorem Ipsum is simply typesetting industry',
+      a: [`Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`, ``,]
+    },
+    {
+      q: 'Lorem Ipsum is simply typesetting industry',
+      a: [`Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`, ``,]
+    },
+    {
+      q: 'Lorem Ipsum is simply typesetting industry',
+      a: [`Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`, ``,]
+    },
+    {
+      q: 'Lorem Ipsum is simply typesetting industry',
+      a: [`Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`, ``,]
+    },
+    {
+      q: 'Lorem Ipsum is simply typesetting industry',
+      a: [`Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`, ``,]
+    },
+]
+export default faqData;
