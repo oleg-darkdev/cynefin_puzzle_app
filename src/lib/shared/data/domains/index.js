e746916455df710d50b0e8425fb64abd3cec1049
DarkDev <darkdev_brand@riseup.net>
2024-10-05 18:17:44 +0300

    basic structure for domains data

---
+++ src/lib/shared/data/domains/index.js
@@ -0,0 +1,19 @@
+import simple from "./simple";
+import complicated from "./complicated";
+import complex from "./complex";
+import chaotic from "./chaotic";
+
+const domains = {
+  all: [
+    simple,
+    complicated,
+    complex,
+    chaotic
+  ],
+  simple,
+  complicated,
+  complex,
+  chaotic
+}
+
+export default domains;
