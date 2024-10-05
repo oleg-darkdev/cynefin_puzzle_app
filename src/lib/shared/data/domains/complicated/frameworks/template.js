35d3bdf264a8d5e9004fe6e77c3cd28224bdaff1
DarkDev <darkdev_brand@riseup.net>
2024-10-05 18:13:34 +0300

    upld data for complicated domain

---
+++ src/lib/shared/data/domains/complicated/frameworks/template.js
@@ -0,0 +1,29 @@
+const frameworkDescription = {
+  promoImg: '/images/domains/complicated/_promo.svg',
+  id: 0,
+  icon: '/images/domains/complicated/_icon.svg',
+  logo: `/images/domains/complicated/_logo.svg`,
+  links: {
+    wiki: {
+      ru: ``,
+      en: ``
+    },
+    offisialSiteLink: `/`,
+    articleLink: `/app`,
+  },
+  cynefinDomain: '#complicated',
+  type: '#framework',
+  title: '',
+  abbr: ``,
+  shortDesc: '',
+  examples: [{title: '', link: '/'}, {title: '', link: '/'}, {title: '', link: '/'}, {title: '', link: '/'},],
+  desc: [
+    '',
+    '',
+    '',
+  ],
+  background: {}
+};
+
+
+export default frameworkDescription
---
+++ src/lib/shared/data/domains/complicated/index.js
@@ -0,0 +1,81 @@
+
+// practices
+// import  from './practices/';
+// import  from './practices/';
+// import  from './practices/';
+// import  from './practices/';
+// import  from './practices/';
+// import  from './practices/';
+// import  from './practices/';
+// import  from './practices/';
+// import  from './practices/';
+// import  from './practices/';
+
+// frameworks
+// import sop from './frameworks/standard_operating_procedure';
+// import sop from './frameworks/standard_operating_procedure';
+// import sop from './frameworks/standard_operating_procedure';
+// import sop from './frameworks/standard_operating_procedure';
+// import sop from './frameworks/standard_operating_procedure';
+// import sop from './frameworks/standard_operating_procedure';
+// import sop from './frameworks/standard_operating_procedure';
+// import sop from './frameworks/standard_operating_procedure';
+// import sop from './frameworks/standard_operating_procedure';
+
+
+
+
+
+const complicatedDomain = {
+  domain: 'Сomplicated',
+  shortDesc: '',
+  desc: [
+    '',
+    '',
+    '',
+  ],
+  brandColorText: 'complicated-domain-textColor',
+  brandColorBg: 'complicated-domain-textColor',
+  align: 'r',
+  promoImg: '/images/domains/complicated_promoImg.svg',
+  id: 0,
+  icon: '',
+  link: '/',
+  favoriteTools: [],
+  frameworks: [ ],
+  practices: [],
+  stats: [
+    {
+      title: 'Frameworks.',
+      count: 0
+    },
+    {
+      title: 'Practices.',
+      count: 0
+    },
+  ],
+  characteristics: {
+    desc: '',
+    values: [
+      {
+        title: '',
+        id: 0,
+        desc: [``]
+      },
+      {
+        title: '',
+        id: 0,
+        desc: [``]
+      },
+      {
+        title: '',
+        id: 0,
+        desc: [``]
+      },
+
+    ]
+  }
+};
+
+
+export default complicatedDomain;
