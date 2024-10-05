637d9350459a23dc970c16871a4c43e36b853945
DarkDev <darkdev_brand@riseup.net>
2024-10-05 18:13:48 +0300

    upld data for nav

---
+++ src/lib/shared/data/navigation.js
@@ -0,0 +1,26 @@
+const navigation = [
+  {
+    title: 'О игре',
+    link: '#about'
+  },
+  {
+    title: 'О доменах',
+    link: '#domains'
+  },
+  {
+    title: 'О авторе',
+    link: '/'
+  },
+  {
+    title: 'F.A.Q.',
+    link: '#faq'
+  },
+  {
+    title: 'Статьи',
+    link: '#blog'
+  },
+// how-to-start
+];
+
+
+export default navigation
