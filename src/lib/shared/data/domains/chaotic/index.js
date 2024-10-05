7950970733849e00b7f8001719ca65c7189b8158
DarkDev <darkdev_brand@riseup.net>
2024-10-05 18:12:15 +0300

    upld data for chaotic domain

---
+++ src/lib/shared/data/domains/chaotic/index.js
@@ -0,0 +1,81 @@
+
+// practices
+// import  from './practices/';
+// import  from './practices/';
+// import  from './practices/';
+// import  from './practices/';
+// import  from './practices/';
+// import  from './practices/';
+// import  from './practices/';
+// import  from './practices/';
+// import  from './practices/';
+// import  from './practices/';
+
+// frameworks
+// import sop from './frameworks/standard_operating_procedure';
+// import sop from './frameworks/standard_operating_procedure';
+// import sop from './frameworks/standard_operating_procedure';
+// import sop from './frameworks/standard_operating_procedure';
+// import sop from './frameworks/standard_operating_procedure';
+// import sop from './frameworks/standard_operating_procedure';
+// import sop from './frameworks/standard_operating_procedure';
+// import sop from './frameworks/standard_operating_procedure';
+// import sop from './frameworks/standard_operating_procedure';
+
+
+
+
+
+const chaoticDomain = {
+  domain: 'Chaotic',
+  shortDesc: '',
+  desc: [
+    '',
+    '',
+    '',
+  ],
+  brandColorText: 'chaotic-domain-textColor',
+  brandColorBg: 'chaotic-domain-textColor',
+  align: 'r',
+  promoImg: '/images/domains/chaotic_promoImg.svg',
+  id: 0,
+  icon: '',
+  link: '/',
+  favoriteTools: [],
+  frameworks: [ ],
+  practices: [],
+  stats: [
+    {
+      title: 'Frameworks.',
+      count: 0
+    },
+    {
+      title: 'Practices.',
+      count: 0
+    },
+  ],
+  characteristics: {
+    desc: '',
+    values: [
+      {
+        title: '',
+        id: 0,
+        desc: [``]
+      },
+      {
+        title: '',
+        id: 0,
+        desc: [``]
+      },
+      {
+        title: '',
+        id: 0,
+        desc: [``]
+      },
+
+    ]
+  }
+};
+
+
+export default chaoticDomain;
---
+++ src/lib/shared/data/domains/chaotic/practices/template copy 3.js
@@ -0,0 +1,31 @@
+const practiceDescription = {
+  promoImg: '/images/domains/chaotic/_promo.svg',
+  id: 0,
+  icon: '/images/domains/chaotic/_icon.svg',
+  logo: `/images/domains/chaotic/_logo.svg`,
+  links: {
+    wiki: {
+      ru: ``,
+      en: ``
+    },
+    offisialSiteLink: `/`,
+    articleLink: `/app`,
+  },
+  cynefinDomain: '#chaotic',
+  type: '#practice',
+  title: '',
+  abbr: ``,
+  shortDesc: '',
+  examples: [{title: '', link: '/'}, {title: '', link: '/'}, {title: '', link: '/'}, {title: '', link: '/'},],
+  desc: [
+    '',
+    '',
+    '',
+  ],
+  background: {}
+
+};
+
+
+
+export default practiceDescription
