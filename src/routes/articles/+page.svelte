1bca6714e027a92e16f1772fe61a92baeb421789
DarkDev <darkdev_brand@riseup.net>
2024-10-05 18:17:18 +0300

    init article page

---
+++ src/routes/articles/+page.svelte
@@ -0,0 +1,141 @@
+<script>
+	import { Footer, Faq,  Cta, } from '$lib/widgets';
+</script>
+
+
+<section class="section s-bloghero">
+  <img src="images/621524e92f2a2271e1db615d_1638993653475-image19.jpg" loading="lazy" alt="" class="coverphoto">
+</section>
+
+<article class="section padding-0">
+  <div class="container c-blogcontent">
+    <div id="backtoblog" class="c-blog-left-parent c-blog-left">
+      <a href="/blog" id="w-node-_5f569c67-6ca0-374e-f5a3-b0bfde9b9d27-7fdb6143" data-w-id="5f569c67-6ca0-374e-f5a3-b0bfde9b9d27" class="backtoblog w-inline-block">
+        <img src="images/607da70b7c6777c116e834ae_arrow.svg" loading="lazy" data-w-id="5f569c67-6ca0-374e-f5a3-b0bfde9b9d28" id="w-node-_5f569c67-6ca0-374e-f5a3-b0bfde9b9d28-7fdb6143" alt="" class="backarrow">
+        <div>Back to blog</div>
+      </a>
+      <div id="sharethis" class="sharethis w-node-_5f569c67-6ca0-374e-f5a3-b0bfde9b9d2b-7fdb6143">
+        <div class="sharetet">Share</div>
+        <div id="socials" class="w-layout-grid shareit-socials">
+          <div class="shareitwrap">
+            <div class="html_dark w-embed">
+              <style>
+                svg:hover {
+                  fill: #655BEF;
+                }
+              </style>
+              <a href="http://twitter.com/share?url=https://slang.com/blog/three-stats-for-shortstaffed-restaurants&amp;text=Three helpful stats for shortstaffed restaurants&amp;via=slang" target="_blank" class="shareit-social-link w-inline-block">
+                <img src="images/607f386607b0be63133622c5_twitter.svg" loading="lazy" width="24" height="24" alt="" class="sharit-social-icon">
+              </a>
+            </div>
+            <div class="html-embed socialpurple w-embed">
+              <style>
+                svg:hover {
+                  fill: #655BEF;
+                }
+              </style>
+              <a href="http://twitter.com/share?url=https://withabound/blog/&amp;text=&amp;via=withabound" target="_blank" class="shareit-social-link w-inline-block">
+                <img src="images/6081b51f9c39c6864e8a42ea_twitter-purple.svg" loading="lazy" width="24" height="24" alt="" class="sharit-social-icon">
+              </a>
+            </div>
+          </div>
+          <div class="shareitwrap">
+            <div class="html_dark w-embed">
+              <a href="https://www.facebook.com/sharer/sharer.php?u=https://slang.com/blog/three-stats-for-shortstaffed-restaurants&amp;text=Three helpful stats for shortstaffed restaurants&amp;via=@slang" target="_blank" class="shareit-social-link w-inline-block">
+                <img src="images/607f386601f3ebd4c1f33f86_facebook2021.svg" loading="lazy" width="24" height="24" alt="" class="sharit-social-icon">
+              </a>
+            </div>
+            <div class="html-embed socialpurple w-embed">
+              <a href="https://www.facebook.com/sharer/sharer.php?u=https://withabound.com/blog/&amp;text=&amp;via=@withabound" target="_blank" class="shareit-social-link w-inline-block">
+                <img src="images/6081b51f8eaed3c2d056e823_facebook2021-purple.svg" loading="lazy" width="24" height="24" alt="" class="sharit-social-icon">
+              </a>
+            </div>
+          </div>
+          <div class="shareitwrap">
+            <div class="html_dark w-embed">
+              <a href="https://www.linkedin.com/sharing/share-offsite/?url=https://slang.com/blog/three-stats-for-shortstaffed-restaurants" target="_blank" class="shareit-social-link w-inline-block">
+                <img src="images/607f3866a8d1ad9110c94be8_linkedin-logo.svg" loading="lazy" width="24" height="24" alt="" class="sharit-social-icon">
+              </a>
+            </div>
+            <div class="html-embed socialpurple w-embed">
+              <a href="https://www.linkedin.com/sharing/share-offsite/?url=https://withabound.com/blog/" target="_blank" class="shareit-social-link w-inline-block">
+                <img src="images/6081b51fd8e820014c67328d_linkedin-logo-purple.svg" loading="lazy" width="24" height="24" alt="" class="sharit-social-icon">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <p class="paragraph-small margin-bottom-0">February 22, 2022</p>
+    <h1 class="h1-blogtitle">Three helpful stats for shortstaffed restaurants</h1>
+    <div class="tag_list _40b">
+      <a id="w-node-_5f569c67-6ca0-374e-f5a3-b0bfde9b9d3d-7fdb6143" href="/tag/restuarants" class="button-tag small w-inline-block">
+        <div>Restuarants</div>
+      </a>
+      <a href="/tag/staffing-2" class="button-tag small w-inline-block">
+        <div>Staffing</div>
+      </a>
+      <a href="/tag/voice-ai-2" class="button-tag small w-inline-block">
+        <div>Voice AI</div>
+      </a>
+    </div>
+    <div class="blog_rich w-richtext">
+      <p>It’s no secret that managing call volume is a huge challenge, especially for short staffed restaurants. This recent New York Times article tells some personal stories that explain just how painful staffing phone is for restaurants these days. One restaurant we’ve worked with could only answer 4% of their phone calls with their staffing levels, and many restaurants are answering zero phone calls — choosing to not answer the phone all together.</p>
+      <p>Every restaurant is different, so you will have to explore the solve that’s right of you — whether that’s adjusting your staffing scheduling, hiring more staff, or exploring technology solutions that can help. That said, here are some trends, derived from real restaurant data, that might help you take the first step forward to dealing with your phone woes.</p>
+      <ul role="list">
+        <li>
+          <strong>Saturdays get 3x the amount of phone calls as Mondays.</strong> Based on our data, call volume varies heavily based on day of the week, and Saturdays are when phone call volume peaks. Tuesdays and Wednesdays are also low volume days.
+        </li>
+        <li>
+          <strong>Holidays can increase your call volume by 20%.</strong> Two days before Valentine’s day, restaurants received around 20% higher call volume than on an average Saturday.
+        </li>
+        <li>
+          <strong>Top 3 reasons why people call.</strong> This of course depends on the type of restaurant, but on average most calls are about reservations. Making a reservation is the #1 reasons why guests call restaurants. The #2 reason is to place takeout orders. And the #3 reason why guests call is to modify existing reservations. Keep this in mind when deciding how to staff your phone!
+        </li>
+      </ul>
+      <h4>
+        <br>Curious for more data?�&nbsp;
+      </h4>
+      <p>Did you find this data helpful? Or are you considering how technology might be able to address helping you improve your customer experience? If so, check out <a href="http://Slang.ai">Slang.ai</a>. </p>
+      <h2>‍</h2>
+    </div>
+    <div class="button--wrap margin-top-64 w-condition-invisible">
+      <a href="https://slang.ai" class="button w-dyn-bind-empty w-button"></a>
+    </div>
+    <div class="author_wrap">
+      <h3 class="h3_authors">Authors</h3>
+      <div class="author_list">
+        <div id="w-node-_5f569c67-6ca0-374e-f5a3-b0bfde9b9d4e-7fdb6143" class="author">
+          <div class="author_photo_wrap">
+            <img src="" loading="lazy" alt="" class="author_photo w-dyn-bind-empty">
+          </div>
+          <div id="w-node-_5f569c67-6ca0-374e-f5a3-b0bfde9b9d51-7fdb6143" class="author_info">
+            <div>Alex Sambvani</div>
+            <div class="author_title w-dyn-bind-empty"></div>
+          </div>
+        </div>
+        <div id="w-node-_5f569c67-6ca0-374e-f5a3-b0bfde9b9d56-7fdb6143" class="separator w-condition-invisible"></div>
+        <div id="w-node-_5f569c67-6ca0-374e-f5a3-b0bfde9b9d57-7fdb6143" class="author w-condition-invisible">
+          <div class="author_photo_wrap">
+            <img src="" loading="lazy" alt="" class="author_photo w-dyn-bind-empty">
+          </div>
+          <div id="w-node-_5f569c67-6ca0-374e-f5a3-b0bfde9b9d5a-7fdb6143" class="author_info">
+            <div class="w-dyn-bind-empty"></div>
+            <div class="author_title w-dyn-bind-empty"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</article>
+
+<Cta />
+
+<Faq />
+
+<Footer />
+
+
+<style>
+
+</style>
