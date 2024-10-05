ee85e970c120a76922ada358e2a0fecd92b0de87
DarkDev <darkdev_brand@riseup.net>
2024-10-05 18:16:25 +0300

    alfa v. structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,66 @@
 <script>
-	// import {  } from '$lib/shared';
-	// import {  } from '$lib/widgets';
+	import { cynefinDomains } from '$lib/shared';
+	import { Footer, Hero, FrameworksSlider, Header, ContactForSales, AboutAuthor, AboutValues, PricingCta, Faq, FrameworksTable, PartnersLogos, AboutOtherGames, Blog, Testimonials, Price, AboutDomain, Stats, Cta, TagsCloud, HowItWork, Features, LaunchSteps} from '$lib/widgets';
+
   // import {  } from '$lib/entities'
 </script>
 
 
 <svelte:head>
-  <title>Title</title>
+  <title>Cynefine game</title>
 </svelte:head>
 
+
+<Header />
+
+<Hero />
+
+<!-- <PartnersLogos /> -->
+
+<FrameworksSlider />
+
+<Features />
+
+<section class="section padding-top-104">
+  <HowItWork />
+  <div class="container">
+    <Stats />
+
+    <a name="domains"></a>
+    <AboutDomain domainData={cynefinDomains.simple}/>
+    <AboutDomain domainData={cynefinDomains.complicated}/>
+    <AboutDomain domainData={cynefinDomains.complex}/>
+    <AboutDomain domainData={cynefinDomains.chaotic}/>
+
+    <FrameworksTable />
+
+    <LaunchSteps />
+  </div>
+</section>
+
+<!-- <TagsCloud /> -->
+
+<PricingCta />
+
+<Price />
+
+<AboutOtherGames />
+
+<AboutAuthor />
+
+<AboutValues />
+
+<!-- <Testimonials /> -->
+
+<Faq>
+  <ContactForSales />
+</Faq>
+
+<Cta />
+
+<Blog />
+
+<Footer />
 
 <style>
 
