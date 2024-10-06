14887dc55c4f496709bf747a9cec2dd2a8aee10e
DarkDev <darkdev_brand@riseup.net>
2024-10-06 15:58:54 +0300

    refactoring btn component

---
+++ src/lib/shared/ui/BlackOutlineBtn.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let text, link;
+</script>
+
+<!-- <div style="display:none" class="monthly">
+  <a href="/" class="button button--dark button--pricing w-button">Get this plan</a>
+</div> -->
+<div style="display:flex" class="yearly">
+  <a href="{link}" class="button button--dark button--pricing w-button">{text}</a>
+</div>
+
+
+<style>
+
+</style>
