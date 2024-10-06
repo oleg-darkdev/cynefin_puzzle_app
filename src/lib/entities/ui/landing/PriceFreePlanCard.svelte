c3befc51ac8445cbb9ec4db03f30c16d3f730a67
DarkDev <darkdev_brand@riseup.net>
2024-10-06 15:57:46 +0300

    refactorimg pricing component

---
+++ src/lib/entities/ui/landing/PriceFreePlanCard.svelte
@@ -0,0 +1,179 @@
+<script>
+  export let data;
+
+  console.log(data.subTitle.length)
+</script>
+
+<div role="listitem" class="w-dyn-item">
+  <div class="price-wrap">
+    <div style="color:hsla(0, 0.00%, 7.84%, 1.00);" class="{data.borderColor} price-top">
+      <img src="{data.promoImg}" loading="lazy" alt="" class="pricing_icon">
+      <div class="price">
+        <!-- <div  style="display:none" class="monthly">
+          <h2 style="color:hsla(0, 0.00%, 7.84%, 1.00)" class="margin-bottom-0 h2-price">$199</h2>
+          <div class="h2 h2-price">/ <span class="h4">mo</span>
+          </div>
+        </div> -->
+        <div style="display:flex" class="yearly">
+          <h2 style="color:hsla(0, 0.00%, 7.84%, 1.00)" class="margin-bottom-0 h2-price">{data.price}</h2>
+          <!-- <div class="h2 h2-price">/ <span class="h4">mo</span></div> -->
+        </div>
+      </div>
+      <div class="price w-condition-invisible">
+        <!-- <div  style="display:none" class="monthly">
+          <h2 class="margin-bottom-0 h2-price">$199</h2>
+        </div> -->
+        <div style="display:flex" class="yearly">
+          <h2 class="margin-bottom-0 h2-price">{data.price}</h2>
+        </div>
+      </div>
+      <h3 style="" class="margin-bottom-0 pricing-plan {data.textColor}">{data.title}</h3>
+      {#if data.subTitle.length > 0 }
+        <p style="color:hsla(0, 0.00%, 7.84%, 1.00)" class="textcolor_white weight_700 margin-bottom-0 paragraph-small text-center">{data.subTitle}</p>
+      {/if}
+    </div>
+
+    <slot></slot>
+
+    <!-- {#if data.subTitle.length > 0 }
+      <div class="tier-pre {data.textColor}">
+        {data.subTitle}
+        <div class="w-condition-invisible">Everything <span class="textcolor_pink">Starter</span> has + </div>
+        <div class="w-condition-invisible">Everything <span class="textcolor_yellow">Growth</span> has + </div>
+        <div class="w-condition-invisible">Everything <span class="textcolor_green">Rocket</span> has + </div>
+      </div>
+    {/if} -->
+    <div>
+      <div class="rtb-price w-richtext">
+        {#each data.available as availableItem}
+          <div class="my-1 {data.availableColor}">{availableItem}</div>
+        {/each}
+
+        {#each data.unavailable as unavailableItem}
+          <div class="my-1 no-price">{unavailableItem}</div>
+        {/each}
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<style>
+.free-price-text {
+  color: var(--free-price-textColor);
+}
+.free-price-border {
+  border-color:var(--free-price-borderColor);
+}
+
+.bussines-price-text {
+  color: var(--bussines-price-textColor);
+}
+.bussines-price-border {
+  border-color:var(--bussines-price-borderColor);
+}
+
+
+.coach-price-text {
+  color: var(--coach-price-textColor);
+}
+.coach-price-border {
+  border-color:var(--coach-price-borderColor);
+}
+
+
+.custom-price-text {
+  color: var(--custom-price-textColor);
+}
+.custom-price-border {
+  border-color:var(--custom-price-borderColor);
+}
+
+
+
+.price-wrap {
+  display: -ms-grid;
+  display: grid;
+  height: 100%;
+  padding: 24px;
+  -webkit-align-content: start;
+  -ms-flex-line-pack: start;
+  align-content: start;
+  grid-auto-columns: 1fr;
+  grid-column-gap: 16px;
+  grid-row-gap: 24px;
+  -ms-grid-columns: 1fr;
+  grid-template-columns: 1fr;
+  -ms-grid-rows: auto auto;
+  grid-template-rows: auto auto;
+  border-radius: 24px;
+  background-color: #fff;
+  box-shadow: 0 4px 32px 0 rgba(0, 0, 0, 0.1);
+}
+
+.price-top {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  padding: 24px 16px;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  -webkit-justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -webkit-align-items: center;
+  -ms-flex-align: center;
+  align-items: center;
+  border-style: solid;
+  border-width: 4px;
+  border-radius: 16px;
+  color: #141414;
+}
+
+@media screen and (min-width: 1440px) {
+ .price-wrap {
+    display: -ms-grid;
+    display: grid;
+    grid-auto-columns: 1fr;
+    grid-column-gap: 16px;
+    grid-row-gap: 24px;
+    -ms-grid-columns: 1fr;
+    grid-template-columns: 1fr;
+    -ms-grid-rows: auto auto;
+    grid-template-rows: auto auto;
+    border-radius: 24px;
+    background-color: #fff;
+    box-shadow: 0 4px 32px 0 rgba(0, 0, 0, 0.1);
+  }
+  .price-top {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    padding: 24px 21px;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    -webkit-box-pack: center;
+    -webkit-justify-content: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
+    border-radius: 16px;
+  }
+  .price-top.price-top_orange {
+    background-color: #fec753;
+  }
+}
+</style>
--- src/lib/widgets/ui/landing/PricingCta.svelte
+++ src/lib/widgets/ui/landing/PricingCta.svelte
@@ -8,14 +8,16 @@
       <img src="images/save20.png" loading="lazy" width="177" alt="" class="pricing_save20">
       <img src="images/asterisk_pricing.svg" loading="lazy" alt="" class="pricing-asterisk">
       <img src="images/sparkle_pricing.svg" loading="lazy" alt="" class="pricing-sparkle">
-      <h1>Pick your plan</h1>
-      <p class="margin-bottom-24">There’s a plan for virtually everyone. Best of all—it’s free to start.</p>
+      <!-- Pick your plan< -->
+      <h1>Выбери свой комплект</h1>
+      <!-- There’s a plan for virtually everyone. Best of all—it’s free to start. -->
+      <p class="margin-bottom-24">Здесь найдется план практически для каждого. Начать можно бесплатно.</p>
       <img src="images/saveMobile.png" loading="lazy" alt="" width="200.5" class="pricing_save20 hide show-landscape">
       <div class="button--wrap button--wrap_center">
         <a id="w-node-ebae6871-58d4-66ca-79ee-ab218867e16b-17db6148"  href="#" class="pricing-toggle-wrap w-inline-block">
-          <div id="w-node-ebae6871-58d4-66ca-79ee-ab218867e16c-17db6148" style="color:rgb(232,116,255)" class="toggletext">Monthly</div>
-          <div id="w-node-_1bcbb844-d25b-d3e0-492b-88613f3b4aae-17db6148" style="color:rgb(255,255,255)" class="toggletext togglewith">Yearly</div>
-          <div id="w-node-ebae6871-58d4-66ca-79ee-ab218867e174-17db6148" style="-webkit-transform:translate3d(65px, 0, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(65px, 0, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(65px, 0, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(65px, 0, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);width:108px" class="toggle-bg"></div>
+          <div id="w-node-ebae6871-58d4-66ca-79ee-ab218867e16c-17db6148" style="color:rgb(232,116,255)" class="toggletext">Покупка</div>
+          <div id="w-node-_1bcbb844-d25b-d3e0-492b-88613f3b4aae-17db6148" style="color:rgb(255,255,255)" class="toggletext togglewith">По подписке</div>
+          <div id="w-node-ebae6871-58d4-66ca-79ee-ab218867e174-17db6148"  class="toggle-bg"></div>
         </a>
       </div>
     </div>
@@ -24,5 +26,66 @@
 
 
 <style>
-
+.toggle-bg {
+  position: absolute;
+  z-index: 0;
+  width: 200px;
+  height: 48px;
+  border-radius: 62px;
+  background-color: #e874ff;
+  -webkit-transform: translate(85px, 0);
+  -ms-transform: translate(85px, 0);
+  transform: translate(85px, 0);
+}
+.toggletext {
+  position: relative;
+  z-index: 1;
+  -webkit-transform: translate(36px, 0);
+  -ms-transform: translate(36px, 0);
+  transform: translate(36px, 0);
+}
+.toggletext.togglewith {
+  -webkit-transform: translate(-42px, 0);
+  -ms-transform: translate(-42px, 0);
+  transform: translate(-42px, 0);
+  color: #fff;
+}
+/*
+// 261px;
+*/
+.pricing-toggle-wrap {
+  position: relative;
+  display: -ms-grid;
+  display: grid;
+  width: 400px;
+  height: 72px;
+  padding: 12px 0;
+  -webkit-box-pack: center;
+  -webkit-justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  justify-items: center;
+  -webkit-align-content: center;
+  -ms-flex-line-pack: center;
+  align-content: center;
+  grid-auto-columns: 1fr;
+  grid-column-gap: 75px;
+  grid-row-gap: 16px;
+  -ms-grid-columns: auto 1fr;
+  grid-template-columns: auto 1fr;
+  -ms-grid-rows: auto;
+  grid-template-rows: auto;
+  border-radius: 100px;
+  background-color: rgba(232, 116, 255, 0.24);
+  line-height: 32px;
+  font-weight: 800;
+  text-decoration: none;
+}
+@media screen and (max-width: 479px) {
+  .pricing-toggle-wrap {
+    grid-column-gap: 0px;
+    -ms-grid-columns: 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
+  }
+}
 </style>
