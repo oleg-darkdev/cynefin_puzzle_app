79199ad5872fc5843e958c094878ccab43bd1a1a
DarkDev <darkdev_brand@riseup.net>
2024-10-06 15:58:39 +0300

    upd content

--- src/lib/widgets/ui/landing/Cta.svelte
+++ src/lib/widgets/ui/landing/Cta.svelte
@@ -10,21 +10,21 @@
         <div class="cta-left-top">
           <!-- Ready to learn more? -->
           <h2 class="margin-bottom-0">Хочешь узнать больше?</h2>
-          <p class="margin-bottom-24 width-max-430">Lorem Ipsum is simply dummy text</p>
+          <p class="margin-bottom-24 width-max-430">Немного прочитать статьи или начать захватывающую партию в настольную игру <span>⁉️</span> </p>
         </div>
         <div class="button--wrap">
-          <a href="/articles" target="_blank" class="button w-button">Статьи</a>
-          <a href="/app" class="button button--dark w-button">Начать игру!</a>
+          <a href="/articles" target="_blank" class="button w-button">📰 Статьи</a>
+          <a href="/app" class="button button--dark w-button">Начать игру 🎲</a>
         </div>
       </div>
       <div class="cta-right">
-        <img src="/images/slang%20Final%20CTA%20Comp.png" loading="lazy" alt="cta image" class="cta-photo">
+        <img src="/images/stable/cta.svg" loading="lazy" alt="cta image" class="cta-photo">
       </div>
     </div>
   </div>
 </section>
 
-
+<!-- slang%20Final%20CTA%20Comp.png -->
 <style>
 
 </style>
