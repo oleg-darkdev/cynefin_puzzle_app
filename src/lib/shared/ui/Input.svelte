43f280214c31975b0da3b54d1458d125a212ffd2
DarkDev <darkdev_brand@riseup.net>
2024-10-08 00:01:51 +0300

    refactoring

--- src/lib/shared/ui/Input.svelte
+++ src/lib/shared/ui/Input.svelte
@@ -3,11 +3,16 @@
 </script>
 
 <div class="w-form">
-    <input class="input-field w-input" maxlength="256" name="email-2" data-name="Email 2" placeholder="Your email" type="email" id="email-2" required="">
-    <input type="submit" data-wait="Please wait..." id="w-node-b16e074b-6acb-c41d-7ea4-3b5c8a8d157b-8a8d153d" class="button button--submit w-button" value="Sign up">
+    <input class="input-field w-input" maxlength="256" name="email-2" placeholder="{data.title}" type="email" required="">
     <img src="images/starYellow.svg" loading="lazy" alt="" class="footer-star">
 </div>
 
+
+<!-- <div class="w-form">
+    <input class="input-field w-input" maxlength="256" name="email-2" data-name="Email 2" placeholder="Your email" type="email" id="email-2" required="">
+    <input type="submit" data-wait="Please wait..." id="w-node-b16e074b-6acb-c41d-7ea4-3b5c8a8d157b-8a8d153d" class="button button--submit w-button" value="Sign up">
+    <img src="images/starYellow.svg" loading="lazy" alt="" class="footer-star">
+</div> -->
 
 <style>
 
