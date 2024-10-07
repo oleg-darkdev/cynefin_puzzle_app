82e5cc1b87fbc744ab5c4c5e5f6db322bb836759
DarkDev <darkdev_brand@riseup.net>
2024-10-07 23:59:17 +0300

    alfa v. component

---
+++ src/lib/shared/ui/UlList.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let listData;
+</script>
+
+<div class="blog_rich w-richtext">
+  <p>
+    {listData.desc}
+  </p>
+
+  <ul role="list">
+    {#each listData.values as {title, desc}}
+      <li class='mb-2'>
+        <strong>{title}</strong>
+        {desc}
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+
+</style>
