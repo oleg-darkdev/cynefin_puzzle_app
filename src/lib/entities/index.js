0e23832e7b293dddd5c55cb3710596408758a6cd
DarkDev <darkdev_brand@riseup.net>
2024-10-07 23:58:00 +0300

    upd api

--- src/lib/entities/index.js
+++ src/lib/entities/index.js
@@ -3,18 +3,19 @@
 import ArticleCard from './ui/landing/ArticleCard.svelte';
 import PriceFreePlanCard from './ui/landing/PriceFreePlanCard.svelte';
 // import ArticleCard from './ui/landing/ArticleCard.svelte';
-// import ArticleCard from './ui/landing/ArticleCard.svelte';
 
 // universal
 import DomainCard from './ui/DomainCard.svelte';
+import DomainStatsCard from './ui/DomainStatsCard.svelte';
+
 
 // app
-// import ArticleCard from './ui/app/ArticleCard.svelte';
-// import ArticleCard from './ui/app/ArticleCard.svelte';
-// import ArticleCard from './ui/app/ArticleCard.svelte';
+import DomainCardApp from './ui/app/DomainCardApp.svelte';
+import ToolCardApp from './ui/app/ToolCardApp.svelte';
+import LibraryCard from './ui/app/LibraryCard.svelte';
 // import ArticleCard from './ui/app/ArticleCard.svelte';
 // import ArticleCard from './ui/app/ArticleCard.svelte';
 // import ArticleCard from './ui/app/ArticleCard.svelte';
 
 
-export { ArticleCard, DomainCard, PriceFreePlanCard };
+export { ArticleCard, DomainCard, PriceFreePlanCard, DomainCardApp, ToolCardApp, DomainStatsCard, LibraryCard };
--- src/lib/shared/index.js
+++ src/lib/shared/index.js
@@ -6,15 +6,24 @@ import {domains as cynefinDomains, promoToolsSet} from './data/domains/';
 import socialLinks from './data/socialLinks';
 import faqData from './data/faqData';
 import articles from './data/articles';
-// import faqData from './data/faqData';
 import prices from './data/prices';
 
+// stores
+import selectedDomain from './data/stores/selectedDomain';
+import selectedTool from './data//stores/selectedTool';
+// import selectedDomain from './data/selectedDomain';
+// import selectedDomain from './data/selectedDomain';
+// import selectedDomain from './data/selectedDomain';
+// import selectedDomain from './data/selectedDomain';
+// import selectedDomain from './data/selectedDomain';
 
 
 // ui
 import TagLink from './ui/TagLink.svelte';
 import Input from './ui/Input.svelte';
 import BlackOutlineBtn from './ui/BlackOutlineBtn.svelte';
+import CloseBtn from './ui/CloseBtn.svelte';
+import UlList from './ui/UlList.svelte';
 
 
-export { navigation, partners, Input, features, prices, cynefinDomains, BlackOutlineBtn, promoToolsSet, socialLinks, faqData, articles, TagLink};
+export { navigation, partners, Input, features, prices, cynefinDomains, selectedDomain, selectedTool, UlList, CloseBtn, BlackOutlineBtn, promoToolsSet, socialLinks, faqData, articles, TagLink};
