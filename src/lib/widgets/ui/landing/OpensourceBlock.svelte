a493641510a6e5c2f272b5a03a9e95aeb303bb65
DarkDev <darkdev_brand@riseup.net>
2024-10-07 07:26:57 +0300

    rename

---
+++ src/lib/widgets/ui/landing/OpensourceBlock.svelte
@@ -0,0 +1,43 @@
+<script>
+
+</script>
+
+<section class="section s-about_hero">
+  <img src="/images/aboutBG_Right.png" loading="lazy" alt="" class="coverphoto cp_about_right">
+  <img src="/images/aboutBG_Left_transparent.png" loading="lazy" alt=""  class="coverphoto cp_about_left">
+  <div class="container c-about-illustrations">
+    <!-- <img src="images/star_aboutIcons.svg" loading="lazy" alt="" class="about-star"> -->
+    <!-- <img src="images/frame_aboutIcons.svg" loading="lazy" alt="" class="about-audio"> -->
+    <img src="/images/sparkle02.svg" loading="lazy" alt="" class="about-sparkle">
+    <!-- <img src="images/about-ear_AboutIllustrations.png" loading="lazy" alt="" class="about_ear"> -->
+    <img src="/images/stable/opensource_left.svg" loading="lazy" alt="" class="about_phone">
+    <!-- <img src="/images/stable/opensource.svg" loading="lazy" alt="" class="about_phone"> -->
+
+    <img src="/images/stable/opensource.svg" loading="lazy" alt="opensource github"  class="about_headphones">
+    <!-- <img src="images/about-headphones_AboutIllustrations.png" loading="lazy" sizes="(max-width: 991px) 175.5px, 351px" alt=""  class="about_headphones"> -->
+  </div>
+  <div id="pagehead" class="container c-center_content">
+    <div class="max-w-2xl center-text">
+      <h1>Открытый <br /> проект.</h1>
+      <p class="margin-bottom-24 about_subtext">
+        Мы стремимся делиться накопленными знаниями с сообщество и сделали проекты открытым, исходный код доступен на <a href="https://github.com/oleg-darkdev/cynefin_app" target="_blank">github</a> .
+      </p>
+      <p class="margin-bottom-24 about_subtext">
+        <!-- Кроме данной настольной игры, мы разработали много и других крутых интересных проектов, неполный список таких проектов доступен ниже: -->
+        Кроме данной настольной игры, мы разработали много и других крутых интересных проектов, неполный список таких проектов доступен по ссылке ниже.
+
+      </p>
+      <!-- <div class="margin-bottom-24 about_subtext">
+
+      </div> -->
+      <div class="button--wrap button--wrap_center">
+        <a href="/" class="button w-button">Полный список проектов 👉</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+</style>
