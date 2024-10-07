5e1f6973d10bc946c0fe2d496d7e08c820c72b76
DarkDev <darkdev_brand@riseup.net>
2024-10-08 00:00:11 +0300

    alfa v. structure

---
+++ src/routes/app/library/domain/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+  import { Input,  UlList, selectedDomain } from '$lib/shared';
+  import { DomainStatsCard, LibraryCard, ToolCardApp } from '$lib/entities';
+</script>
+
+<svelte:head>
+  <title>CynefiPuzzle - linrary</title>
+</svelte:head>
+
+<section class="w-dyn-list w-full mb-10">
+  <div class=" max-w-2xl mx-auto">
+    <LibraryCard domain={$selectedDomain}>
+      <div slot='body'>
+        <DomainStatsCard domainData={$selectedDomain}/>
+        <UlList listData={$selectedDomain.characteristics}/>
+      </div>
+    </LibraryCard>
+  </div>
+</section>
+
+<section class="w-full mx-auto max-w-3xl">
+  <Input data={{title: 'Поиск среди фреймоворков'}}/>
+
+  <h3 class="margin-top-24 margin-bottom-24">
+    Список фреймворк в домене {$selectedDomain.domain}
+  </h3>
+  <div role="list" class="tool-list w-dyn-items">
+    {#each $selectedDomain.frameworks as tool}
+      <ToolCardApp {tool} />
+    {/each}
+  </div>
+</section>
+
+<section class="w-full mx-auto max-w-3xl">
+  <Input data={{title: 'Поиск среди практик'}}/>
+
+  <h3 class="margin-top-24 margin-bottom-24">
+    Список практик в домене {$selectedDomain.domain}
+  </h3>
+  <div role="list" class="tool-list w-dyn-items">
+    {#each $selectedDomain.practices as tool}
+      <ToolCardApp {tool} />
+    {/each}
+  </div>
+</section>
+
+<style>
+
+</style>
