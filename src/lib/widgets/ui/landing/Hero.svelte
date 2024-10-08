938d375e3b3c3fe5eb711c24a0500434f55aa7b6
DarkDev <darkdev_brand@riseup.net>
2024-10-08 03:29:30 +0300

    upd content

--- src/lib/widgets/ui/landing/Hero.svelte
+++ src/lib/widgets/ui/landing/Hero.svelte
@@ -7,15 +7,15 @@
 <section class="section s-home_hero">
   <img src="/images/background_SlangHeroBG_Comp.jpg" loading="eager" alt="" class="coverphoto_bghero">
   <div class="hero_illustrations">
-    <img src="/images/bot%20left_SlangHero.png" loading="lazy" alt="" width="206" class="hero_botleft">
-    <img src="/images/musician_SlangHero.png" loading="lazy" width="194" alt="" class="hero_musician">
-    <img src="/images/girl%20Walking_SlangHero.png" loading="lazy" alt="" width="222" class="hero_girlwalking">
-    <img src="/images/bot%20Right_SlangHero.png" loading="lazy" alt="" width="164" class="hero_botright">
+    <img src="/images/stable/hero_puzzle_left.svg" loading="lazy" alt="" width="206" class="hero_botleft">
+    <!-- <img src="/images/musician_SlangHero.png" loading="lazy" width="194" alt="" class="hero_musician"> -->
+    <!-- <img src="/images/girl%20Walking_SlangHero.png" loading="lazy" alt="" width="222" class="hero_girlwalking"> -->
+    <img src="/images/stable/hero_puzzle_right.svg" loading="lazy" alt="" width="164" class="hero_botright">
     <div class="container c-hero_illo">
-      <div class="hero_skateboarderwrap">
+      <!-- <div class="hero_skateboarderwrap">
         <img src="/images/skateboarder_SlangHero.png" loading="lazy" alt="" width="268"  sizes="(max-width: 767px) 166.59375px, 238px" class="hero_skateboarder_image">
         <div class="hero_skateboarder_line"></div>
-      </div>
+      </div> -->
       <div class="hero_dogwrap">
         <img src="/images/dog_SlangHero.png" loading="lazy" alt="" width="162" class="hero_dog_image">
         <div class="hero_dog_line"></div>
@@ -24,33 +24,29 @@
     <div class="hero_bottomline"></div>
   </div>
   <div class="container c-center_content">
-    <div class="width-max-620 center-text c_hero">
+    <div class="max-w-3xl center-text c_hero">
       <h1 class="h1_hero">
-        Lorem Ipsum is simply dummy text
+        Cynefin Puzzle — обучающая настольная игра.
       </h1>
       <p class="p_hero">
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        Cозданна для глубокого понимания модели Cynefin, предлагает игрокам погрузиться в процесс принятия решений через призму пяти доменов модели Cynefin: <a href='#simple-domain' class="simple-domain-textColor">Простой</a>, <a href='#complicated-domain' class="complicated-domain-textColor">Сложный</a>, <a href='#complex-domain' class="complex-domain-textColor">Комплексный</a>, <a href='#chaotic-domain' class="chaos-domain-textColor">Хаотический</a>, и <a href='#disorder-domain' class='disorder-domain-textColor'>Беспорядочный</a>.
       </p>
       <div class="button--wrap button--wrap_center margin-top-40">
-        <a href="/" target="_blank" class="button w-button">Learn More</a>
-        <div  class="playbutton_wrap">
-          <div class="audio-player-button">
-            <div class="control-wrapper small">
-              <div player="play-pause" class="play-wrapper is-grid">
-                <img src="/images/player.svg" height="" alt="" hide-if="playing" class="play">
-                <img src="/images/pauseTeal.svg" height="" alt="" show-if="playing" class="pause">
-                <div hide-if="playing" class="textcolor_pink">About</div>
-                <div show-if="playing" class="textcolor_green">cynefin.</div>
-              </div>
-            </div>
-            <div hide-if="playing" class="background_pink background-hearit">
-              <div class="playbutton_pinkborder"></div>
-            </div>
-            <div show-if="playing" class="background_green background-pauseit">
-              <div class="playbutton_tealborder"></div>
-            </div>
+        <a href="#about" class="button w-button">Об игре 🎲</a>
+
+        <div  class="playbutton_wrap ">
+        <div  class="audio-player-button h-16">
+          <div class="control-wrapper small">
+            <a href='/app' target="_blank" class="no-underline play-wrapper is-grid">
+              <img src="/images/player.svg"  alt="" class="h-8">
+              <span  class="textcolor_pink">Play the game!</span>
+            </a>
+          </div>
+          <div class="background_pink background-hearit">
+            <div class="playbutton_pinkborder"></div>
           </div>
         </div>
+      </div>
       </div>
     </div>
   </div>
@@ -58,5 +54,66 @@
 
 
 <style>
+.hero_botleft {
+  position: absolute;
+  left: auto;
+  top: 104px;
+  right: auto;
+  bottom: auto;
+  z-index: 3;
+  width: 206px;
+  margin-left: -859px;
+}
+.hero_botright {
+  position: absolute;
+  left: auto;
+  top: 85px;
+  right: auto;
+  bottom: auto;
+  z-index: 3;
+  width: 289px;
+  margin-left: 801px;
+}
+@media screen and (min-width: 1440px) {
+  .hero_botleft {
+    top: 117px;
+    margin-left: -1075px;
+  }
+  .hero_botright {
+    margin-left: 980px;
+  }
+}
+@media screen and (max-width: 991px) {
+  .hero_botleft {
+    max-width: 103px;
+    margin-left: -734px;
+  }
+  .hero_botright {
+    top: 97px;
+    max-width: 82px;
+    margin-left: 713px;
+  }
+}
+@media screen and (max-width: 767px) {
+  .hero_botleft {
+    left: 16px;
+    top: 97px;
+    margin-left: 0;
+  }
+  .hero_botright {
+    top: 87px;
+    right: 24px;
+  }
+}
 
+@media screen and (max-width: 479px) {
+  .hero_botleft {
+    left: 16px;
+    margin-left: 0;
+  }
+  .hero_botright {
+    top: 99px;
+    right: 16px;
+  }
+}
 </style>
